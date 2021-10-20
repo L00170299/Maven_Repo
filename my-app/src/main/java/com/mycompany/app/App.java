14046f700e16378196b4e183f4ba2ed86cc7e867
L00170299 <L00170299@student.lyit.ie>
2021-10-20 21:16:20 +0100

    Maven test1

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,21 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( getHelloWorld() );
+    }
+
+    public static int getNumber10(){
+        return 10;
+    }
+
+    public static String getHelloWorld(){
+        return "Hello World!";
+    }
+}
---
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,38 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithFalse()
+    {
+        assertFalse( false );
+    }
+
+    @Test
+    public void testNumber10() {
+        assertEquals(10, App.getNumber10());
+    }
+
+    @Test
+    public void testHelloWorld() {
+        assertEquals("Hello World!", App.getHelloWorld());
+    }
+}
